514d294a54e97396a1ff5ca8362260010237530a
chirag-droid <chirag.singla.pi@gmail.com>
2024-04-04 15:53:35 +0530

    Add a basic snake sprite

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+mod snake;
+
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_systems(Startup, setup)
+        .add_plugins(DefaultPlugins)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    println!("Welcome to Snake Game in Rust!");
+
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(0.5, 0.5, 0.0),
+                scale: Vec3::new(20.0, 20.0, 0.0),
+                ..default()
+            },
+            sprite: Sprite {
+                color: Color::hsl(0.0, 0.0, 0.8),
+                ..default()
+            },
+            ..default()
+        },
+        snake::Snake
+    ));
 }
